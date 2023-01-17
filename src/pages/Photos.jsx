3ae187442b942145339eb7d1d71075dfc2f656e4
katoragashua <katoragashua@outlook.com>
2023-01-17 15:43:50 +0100

    UI Changes: Fixed grid in Photos page

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -5,11 +5,26 @@ import { Context } from "../Context";
 const Photos = (props) => {
   const { photos, incrementPageNum } = useContext(Context);
 
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
   const allImages = photos.map((img) => <Img key={img.id} img={img} />);
 
   return (
     <div style={{ textAlign: "center", padding: "1em 0" }}>
-      <div className="photos-page container">{allImages}</div>
+      <div className="photos-page container">
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {allImages}
+        </Masonry>
+      </div>
       <span className="load-more" onClick={incrementPageNum}>
         Load more
       </span>
