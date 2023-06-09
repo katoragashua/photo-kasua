a6da9849ebb1854e20763211ec35901b92ccf007
Kator Agashua <73258040+katoragashua@users.noreply.github.com>
2023-06-09 13:46:35 +0100

    Update main.jsx
    Fix Context in main

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import {ContextProvider} from './Context.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<ContextProvider>
-  <BrowserRouter>
-     <React.StrictMode>
-        <App />
-    </React.StrictMode> 
-  </BrowserRouter>
-</ContextProvider>
+  <React.StrictMode>
+    <ContextProvider>
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
+   </ContextProvider>
+  </React.StrictMode> 
 )
